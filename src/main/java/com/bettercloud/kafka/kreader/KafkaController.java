8ebd7b93b2625950b1683ce9afe27485049f29b4
kenkummerfeldt <ken.kummerfeldt@bettercloud.com>
2018-10-19 13:21:35 -0400

    add filtering and serialization option

--- src/main/java/com/bettercloud/kafka/kreader/KafkaController.java
+++ src/main/java/com/bettercloud/kafka/kreader/KafkaController.java
@@ -6,14 +6,11 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
@@ -32,6 +29,10 @@ import java.util.stream.Collectors;
 public class KafkaController {
     private static final Duration READ_TIMEOUT = Duration.ofSeconds(5);
     private static final String SCHEMA_REGISTRY_URL_CONFIG = "schema.registry.url";
+    private static final String AVRO_DESERIALIZAER = "Avro";
+    private static final String STRING_DESERIALIZAER = "String";
+    private static final String LONG_MAX_VALUE = "9223372036854775807";
+    private static final int MAX_SIZE = 10000;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final String bootstrapServers;
     private final String schemaRegistryUrl;
@@ -45,23 +46,36 @@ public class KafkaController {
 
     @GetMapping("/topics/{topic}/partitions/{partition}")
     public Mono<ResponseEntity<Object>> records(@PathVariable String topic,
-                                        @PathVariable int partition,
-                                        @RequestParam(defaultValue = "100") int size,
-                                        @RequestParam(defaultValue = "0") long offset) {
-        if(size > 10000)
+                                                @PathVariable int partition,
+                                                @RequestParam(defaultValue = AVRO_DESERIALIZAER) String serialization,
+                                                @RequestParam(defaultValue = "") String filter,
+                                                @RequestParam(defaultValue = LONG_MAX_VALUE) Long before,
+                                                @RequestParam(defaultValue = "0") Long after,
+                                                @RequestParam(defaultValue = "10") int size,
+                                                @RequestParam(defaultValue = "0") long offset) {
+        if (size > MAX_SIZE)
             return Mono.just(ResponseEntity.badRequest().body("Query param 'size' must be less than " + 10000));
+
+        Object deserializer = KafkaAvroDeserializer.class;
+
+        if (serialization.equals(STRING_DESERIALIZAER))
+            deserializer = StringDeserializer.class;
 
         final Map<String, Object> consumerProps = new HashMap<>();
         consumerProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         consumerProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
+        consumerProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+
 
         final ReceiverOptions<String, Object> receiverOptions = ReceiverOptions.<String, Object>create(consumerProps)
                 .consumerProperty(SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl)
                 .addAssignListener(assignedPartitions -> assignedPartitions.forEach(p -> p.seek(offset)))
                 .assignment(Collections.singletonList(new TopicPartition(topic, partition)));
+
         return KafkaReceiver.create(receiverOptions)
                 .receive()
+                .filter(record -> record.toString().contains(filter))
+                .filter(record -> record.timestamp() < before && record.timestamp() > after)
                 .take(size)
                 .take(READ_TIMEOUT)
                 .collectList()
